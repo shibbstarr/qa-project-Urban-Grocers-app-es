bd16faa7157aa658c9980bd08f30ea2aa6205bbf
OSCAR QUINTERO <adrianquintero23951@gmail.com>
2026-07-07 01:33:39 -0600

    Elimina archivos ajenos de otro proyecto y deja de rastrear __pycache__

--- configuration.py
+++
@@ -1,5 +0,0 @@
-
-URL_SERVICE = "https://cnt-60112bb1-03f0-488d-b287-3876872fbf62.containerhub.tripleten-services.com"
-# Rutas de los Endpoints dr las API DOCS
-CREATE_USER_PATH = "/api/v1/users"
-CREATE_KIT_PATH = "/api/v1/kits"
